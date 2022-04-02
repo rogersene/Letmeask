d46cd12efe6c4ef81dcf7ea212448c6cdca4bcc6
rogersene <rogersenefaria@gmail.com>
2022-04-02 13:26:32 -0300

    refactored authentication context and applied best practices

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,20 @@
-import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import ilustrationImg from '../assets/images/illustration.svg';
-import { auth, firebase } from '../services/firebase';
 import logoImg from '../assets/images/logo.svg';
 import gooogleIconImg from '../assets/images/google-icon.svg';
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 export function Home() {
   const navigate = useNavigate();
-
-  function handleCreateRoom() {
+  const { user, signInWithGoogle } = useAuth()
+ 
+  async function handleCreateRoom() {
+      if (!user) {
+        await signInWithGoogle()
+      }
       navigate('/rooms/new')
-
-    })
-
   }
 
   return(
@@ -25,7 +25,6 @@ export function Home() {
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
       <main>
-        <h1>{value}</h1>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
           <button className="create-room" onClick={handleCreateRoom}>
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -3,8 +3,10 @@ import ilustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import '../styles/auth.scss';
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 export function NewRoom() {
+  const { user} = useAuth()
 
   return(
     <div id="page-auth">

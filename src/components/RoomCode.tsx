658650ad140b2b0128112630887e54e10050e0b7
rogersene <rogersenefaria@gmail.com>
2022-04-12 05:41:54 -0300

    finished pasge stylization and implemented question feature

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,13 +1,22 @@
 import '../styles/room-code.scss'
 import copyImg from '../assets/images/copy.svg'
 
-export function RoomCode() {
+
+type RoomCodeProps = {
+  code: string;
+
+}
+
+export function RoomCode(props: RoomCodeProps) {
+  function copyRoomCodeToClipboard() {
+    navigator.clipboard.writeText(props.code)
+  }
   return (
-    <button className="room-code">
+    <button className="room-code" onClick={copyRoomCodeToClipboard}>
       <div>
         <img src={copyImg} alt="Copy room code" />
       </div>
-      <span>Sala #dgkgkgmkkmfgdkgdk</span>
+      <span>Sala #{props.code}</span>
     </button>
   )
 }
